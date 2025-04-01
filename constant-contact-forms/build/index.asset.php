a41500fc5e3b18dac8924cd77689c1dc5ed6c993
Sean Murphy <sean@iamseanmurphy.com>
2025-04-01 18:19:11 +0000

    2.9.0

--- constant-contact-forms/build/index.asset.php
+++ constant-contact-forms/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n', 'wp-server-side-render'), 'version' => 'a0c93a48fe45acb6c3c9');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n', 'wp-server-side-render'), 'version' => 'ea63e29ebd4546ad266d');
